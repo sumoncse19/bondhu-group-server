72dcc3037c0876d9642fe37d756da3915150e9d2
sumoncse19 <mdsumoncse19@gmail.com>
2024-10-02 03:37:24 +0600

    add purchase only superAdmin and add role field in getAllUser

--- src/app/modules/purchase/purchase.routes.ts
+++ src/app/modules/purchase/purchase.routes.ts
@@ -3,12 +3,13 @@ import validateRequest from '../../middleware/validateRequest'
 import requireAuth from '../../middleware/requireAuth'
 import { PurchaseMoneyControllers } from './purchase.controller'
 import { purchaseMoneySchema } from './purchase.schema'
+import { Roles } from '../shared/user.enumeration'
 
 const router = express.Router()
 
 router.post(
   '/',
-  requireAuth(),
+  requireAuth(Roles.SUPER_ADMIN),
   validateRequest(purchaseMoneySchema),
   PurchaseMoneyControllers.purchaseMoney,
 )

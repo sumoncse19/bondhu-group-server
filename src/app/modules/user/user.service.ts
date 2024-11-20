d8f43a8f60f90f6fb5df16f08ea469fb29bf8c55
sumoncse19 <mdsumoncse19@gmail.com>
2024-11-21 02:07:06 +0600

    reverse getAllUserApi

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -365,6 +365,7 @@ const getAllUserFromDB = async (
     : {}
 
   const users = await UserModel.find({ ...searchCondition })
+    .sort({ _id: -1 })
     .select(
       '_id name user_name serial_number email role designation phone reference_id parent_placement_id left_side_partner right_side_partner registration_date picture nid_passport_no choice_side nominee_name is_approved',
     )
